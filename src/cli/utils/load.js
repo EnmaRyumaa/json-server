d658bd296a0ee5b8453ef6b380d72ae0abf9083f
Fernando Auguste <fer.augustofs@gmail.com>
2023-06-22 14:12:37 -0300

    return const example to origin

--- src/cli/utils/load.js
+++ src/cli/utils/load.js
@@ -9,15 +9,9 @@ const is = require('./is')
 const chalk = require('chalk')
 
 const example = {
-  user: 
-  [
-    {
-      name:"admin", 
-      lastname: "admin", 
-      email: "admin@gmail.com", 
-      password: "password"
-    }
-  ]
+  posts: [{ id: 1, title: 'json-server', author: 'typicode' }],
+  comments: [{ id: 1, body: 'some comment', postId: 1 }],
+  profile: { name: 'typicode' },
 }
 
 module.exports = function (source) {
